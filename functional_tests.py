3965ca99bfddc65d277d7b9a3e9aff62994b56a8
kreiswelt <niwra2000@yahoo.com>
2015-07-28 17:17:29 +0800

    Red/Green/Refactor and Triangulation

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Dede has heard about a cool new online to-do app. He goes
 		# to check out its homepage
@@ -37,18 +42,24 @@ class NewVisitorTest(unittest.TestCase):
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertTrue(
 			any(row.text == '1: Buy peacock feathers' for row in rows),
-			"New to-do item did not appear in table"
+			"New to-do item did not appear in table -- its text was:\n%s" % (
+				table.text)
 		)
 
 		# There is still a text box showing him to add another item. He
 		# enters "Use peacock feathers to make a fly" (Dede is very methodical)
-		self.fail('Finish the test!')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
 
 		# The page updates again, and now shows both items on his list
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 		# Dede wonders whether the site will remember his list. Then he sees
 		# that the site has generated a unique URL for her -- there is some
 		# explanatory text to that effect.
+		self.fail('Finish the test!')
 
 		# He visits that URL - his to-do list is still there.
 
